c80e42a13d358447ff60e3bc0c7debabbba34993
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-19 20:39:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      
+      if @@cart.length == 0
+        resp.write "Your cart is empty."
+      end
     else
       resp.write "Path Not Found"
     end
